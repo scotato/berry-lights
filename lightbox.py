d55854d8a94ab174233d675f04d5075ea0fc3d47
Scott Dodge <scott@goldilocks.design>
2020-10-03 23:27:57 -0500

    add library and sample code

---
+++ lightbox.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import time
+from rpi_ws281x import PixelStrip, Color
+import argparse
+
+# LED strip configuration:
+LED_COUNT = 42        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+# Lights for a lightbox using first and last 14 LEDs in a 42 LED strip
+def lightbox(strip, wait_ms=50):
+    """Fill first and last 14 LEDs with white."""
+    for i in range(14):
+	strip.setPixelColor(i, Color(255, 255, 255))
+	strip.show()
+	time.sleep(wait_ms / 1000.0)
+
+    for i in range(14):
+	strip.setPixelColor(i + 14, Color(0, 0, 0))
+	strip.show()
+
+    for i in range(14):
+	strip.setPixelColor(i + 28, Color(255, 255, 255))
+	strip.show()
+	time.sleep(wait_ms / 1000.0)
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+	lightbox(strip)
+
+    except KeyboardInterrupt:
+        if args.clear:
+            colorWipe(strip, Color(0, 0, 0), 10)
